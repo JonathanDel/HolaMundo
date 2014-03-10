dde86d654a04e39b4fc99e70fb4d0cf847e981ee
amunoz <amunoz@Ganimedes.lan>
2014-03-10 11:37:40 -0600

    Prueba de hola mundo

--- src/test/Test.java
+++ src/test/Test.java
@@ -16,7 +16,7 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("que onda");
     }
     
 }
